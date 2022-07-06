dfa9dda420978fbfaf594d0c9b552dd548667762
Andreas Jonasson <officialskum@gmail.com>
2022-07-06 21:56:22 +0200

    first push to project

--- App.js
+++ App.js
@@ -1,20 +1,87 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ActivityIndicator,
+  FlatList,
+  Image,
+} from "react-native";
 
-export default function App() {
+const mealRouletteURL =
+  "https://playground.devskills.co/api/rest/meal-roulette-app/meals/limit/4/offset/4";
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(mealRouletteURL)
+      .then((response) => response.json()) //hämta och hantera JSON
+      .then((json) => {
+        setData(json.meal_roulette_app_meals_aggregate.nodes);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <View>
+          <View style={{ borderBottomWidth: 1, marginBottom: 12 }}></View>
+          <FlatList
+            data={data}
+            keyExtractor={({ id }, index) => id}
+            renderItem={({ item }) => (
+              <View style={{ paddingBottom: 10 }}>
+                <Image
+                  style={styles.tinyLogo}
+                  source={{
+                    uri: `${item.picture}`,
+                  }}
+                />
+                <Text style={styles.mealText}>
+                  {item.id}. {item.title}
+                </Text>
+              </View>
+            )}
+          />
+        </View>
+      )}
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    marginTop: 48,
+  },
+  mealText: {
+    fontSize: 26,
+    fontWeight: "200",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  description: {
+    textAlign: "center",
+    marginBottom: 18,
+    fontWeight: "200",
+    color: "green",
+  },
+  tinyLogo: {
+    width: 150,
+    height: 150,
   },
 });
+
+export default App;
